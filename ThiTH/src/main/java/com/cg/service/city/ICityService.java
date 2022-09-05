fab2eb243b560cca64c7c507429b39e7410b5543
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-09-05 08:18:50 +0700

    Up

--- ThiTH/src/main/java/com/cg/service/city/ICityService.java
+++
@@ -1,7 +0,0 @@
-package com.cg.service.city;
-
-import com.cg.model.City;
-import com.cg.service.IGeneralService;
-
-public interface ICityService extends IGeneralService<City> {
-}
--- ThiTH/src/main/java/com/cg/service/nation/INationService.java
+++
@@ -1,7 +0,0 @@
-package com.cg.service.nation;
-
-import com.cg.model.Nation;
-import com.cg.service.IGeneralService;
-
-public interface INationService extends IGeneralService<Nation> {
-}
