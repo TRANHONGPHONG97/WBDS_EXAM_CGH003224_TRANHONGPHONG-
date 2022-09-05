e81dce08837080541195ce4b85e057b0aca53367
TRANHONGPHONG97 <phong12b5nd@gmail.com>
2022-09-05 09:55:20 +0700

    Up

---
+++ WBDS_EXAM_CGH003224_TRANHONGPHONG/src/main/java/com/cg/model/CityDTO.java
@@ -0,0 +1,49 @@
+package com.cg.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Accessors(chain = true)
+public class CityDTO {
+    private Long id;
+
+    @NotBlank(message = "Tên thành phố không được để trống!")
+    @Size(min = 5, max = 30, message = "Tên thành phố phải từ 5-30 kí tự!")
+    private String cityName;
+
+    @NotBlank(message = "Tên quốc gia không được để trống!")
+    private String nation;
+
+    @NotBlank(message = "Diện tích không được để trống!")
+    private String area;
+
+    @NotBlank(message = "Dân số không được để trống!")
+    private String population;
+
+    @NotBlank(message = "GDP không được để trống!")
+    private String gdp;
+
+    @NotBlank(message = "Mô tả không được để trống!")
+    @Size(min = 5, max = 255, message = "Mô tả từ 5 - 255 kí tự")
+    private String description;
+
+    public City toCity(Nation nation) {
+        return new City()
+                .setCityName(cityName)
+                .setNation(nation)
+                .setArea(Integer.parseInt(area))
+                .setGdp(Integer.parseInt(gdp))
+                .setPopulation(Integer.parseInt(population))
+                .setDescription(description);
+    }
+}
